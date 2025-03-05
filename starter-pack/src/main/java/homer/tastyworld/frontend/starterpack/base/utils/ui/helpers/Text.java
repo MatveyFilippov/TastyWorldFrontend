d1617467071b9418632ae15f7f54211fc3001766
MatveyFilippov <matdufi2004@gmail.com>
2025-03-05 03:59:32 +0300

    text size bug fix

--- starter-pack/src/main/java/homer/tastyworld/frontend/starterpack/base/utils/ui/helpers/Text.java
+++ starter-pack/src/main/java/homer/tastyworld/frontend/starterpack/base/utils/ui/helpers/Text.java
@@ -5,6 +5,7 @@ import javafx.beans.binding.StringExpression;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.scene.text.TextAlignment;
 
@@ -21,6 +22,7 @@ public class Text {
     public static void setTextLeft(AnchorPane pane, String text, StringExpression fontSize, Color color) {
         Label label = new Label(text);
         label.setWrapText(false);
+        label.setPrefSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
         AnchorPane.setTopAnchor(label, 0.0);
         AnchorPane.setBottomAnchor(label, 0.0);
         if (color != null) {
@@ -35,6 +37,7 @@ public class Text {
         label.setTextAlignment(TextAlignment.CENTER);
         label.setAlignment(Pos.CENTER);
         label.setWrapText(false);
+        label.setPrefSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
         AnchorPane.setTopAnchor(label, 0.0);
         AnchorPane.setBottomAnchor(label, 0.0);
         AnchorPane.setLeftAnchor(label, 0.0);
