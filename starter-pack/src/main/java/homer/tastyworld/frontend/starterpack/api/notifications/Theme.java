f4361741f878d859b5a73049b8581c3dfa1f93f1
MatveyFilippov <matdufi2004@gmail.com>
2025-03-12 02:33:35 +0300

    update notification queue names

--- starter-pack/src/main/java/homer/tastyworld/frontend/starterpack/api/notifications/Theme.java
+++ starter-pack/src/main/java/homer/tastyworld/frontend/starterpack/api/notifications/Theme.java
@@ -3,16 +3,19 @@ package homer.tastyworld.frontend.starterpack.api.notifications;
 import homer.tastyworld.frontend.starterpack.base.config.AppConfig;
 import homer.tastyworld.frontend.starterpack.api.requests.MyParams;
 import homer.tastyworld.frontend.starterpack.base.utils.misc.SHA256;
+import homer.tastyworld.frontend.starterpack.base.utils.misc.TypeChanger;
 
 public enum Theme {
 
     ORDER_STATUS_CHANGED,
     ORDER_PAID_MARKED;
 
-    private static final String CLIENT_POINT_NAME = (String) MyParams.getClientPointInfo().get("NAME");
+    private static final String QUEUE_STARTS_WITH = String.format(
+            "ClientPoint_%s.", TypeChanger.toLong(MyParams.getClientPointInfo().get("ID"))
+    );
 
     String getQueueName() {
-        return CLIENT_POINT_NAME.replace(" ", "_") + "." + this.name() + "." + SHA256.hash(AppConfig.getToken());
+        return QUEUE_STARTS_WITH + this.name() + "." + SHA256.hash(AppConfig.getToken());
     }
 
 }
